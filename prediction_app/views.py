9f8c05030215721a8546e4e53cfbeb17b1a666f9
mouaad-el <elrahmanimouaad@gmail.com>
2026-07-15 13:08:36 +0100

    Fix login import in views

--- prediction_app/views.py
+++ prediction_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import login
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 import joblib
